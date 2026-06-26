72f88bc4724455dbb9e9b75e279c0259f1e33bba
sisoyjairotorres-cloud <sisoyjairotorres@gmail.com>
2026-06-26 17:29:58 -0400

    Añadir modelos vacíos y migraciones normalizadas en inglés

---
+++ database/migrations/2026_06_26_212057_create_available_classes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('available_classes', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('subject_id')->constrained('subjects')->cascadeOnDelete();
+                $table->foreignId('teacher_id')->constrained('teachers')->cascadeOnDelete();
+                $table->foreignId('classroom_id')->constrained('classrooms')->cascadeOnDelete();
+                $table->foreignId('time_slot_id')->constrained('time_slots')->cascadeOnDelete();
+                $table->foreignId('semester_id')->constrained('semesters')->cascadeOnDelete();
+                $table->foreignId('group_id')->constrained('groups')->cascadeOnDelete();
+                $table->foreignId('specialty_id')->nullable()->constrained('specialties')->nullOnDelete();
+                $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('available_classes');
+    }
+};
---
+++ database/migrations/2026_06_26_212100_create_saved_schedules_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saved_schedules', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        $table->string('nombre_horario');
+        $table->string('gestion');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saved_schedules');
+    }
+};
